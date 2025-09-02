f9346aa9d18170eaf67b21bffb0f92eaef2a5fc3
Sebas <elsebaslml@gmail.com>
2025-09-01 23:19:34 -0500

    feature/1998 Inicializacion de proyecto

---
+++ src/main/java/com/jsca/demo/GestionGastosPersonalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.jsca.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionGastosPersonalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionGastosPersonalesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsca/demo/GestionGastosPersonalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsca.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionGastosPersonalesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
